ac135c4158b5ee285cc344ccbb3b023fad3b562a
ElementMo <869077498@qq.com>
2018-01-26 20:03:20 +0800

    Init

---
+++ XML_Read/Assets/ReadXML.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.IO;
+
+
+public class ReadXML : MonoBehaviour {
+	public GameObject cube;
+
+
+	float x=0;
+	float z=0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		XmlDocument xml = new XmlDocument ();
+		xml.Load (XmlReader.Create("Assets/HousePos.xml"));
+		XmlNodeList xmlPoses = xml.SelectSingleNode("Poses").ChildNodes;
+		foreach (XmlElement xmlPos in xmlPoses) 
+		{
+			foreach (XmlElement xAndy in xmlPos) 
+			{
+				if (xAndy.Name == "xpos") 
+				{
+					x = float.Parse(xAndy.InnerText);
+				}
+				if (xAndy.Name == "ypos") 
+				{
+					z = float.Parse(xAndy.InnerText);
+				}
+			}
+			Vector3 position = new Vector3 (x, 0, 4000-z);
+			Instantiate (cube, position, Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
